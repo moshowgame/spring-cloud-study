16c9aec65dae0b2e7566660c5a3bf4623ef41fd6
moshowgame <moshowgame@126.com>
2018-06-01 23:43:20 +0800

    add CorsFilter&ignored-headers for zuul

--- spring-cloud-study-zuul/src/main/java/com/softdev/system/zuul/config/GateWayCorsConfig.java
+++ spring-cloud-study-zuul/src/main/java/com/softdev/system/zuul/config/GateWayCorsConfig.java
@@ -1,4 +1,4 @@
-package com.softdev.system.zuul;
+package com.softdev.system.zuul.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
