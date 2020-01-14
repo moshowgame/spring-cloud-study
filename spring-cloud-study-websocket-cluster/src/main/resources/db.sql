eed944cc8d92178e770a0b464dea1c40b0e62e6a
MOSHOW.K.ZHENG <moshowgame@126.com>
2020-01-15 00:00:49 +0800

    websocket-cluster(undone)

---
+++ spring-cloud-study-websocket-cluster/src/main/resources/db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `ws_message` (
+  `id` varchar(100) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'UUID()',
+  `from_user_id` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `to_user_id` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `message` text COLLATE utf8mb4_general_ci,
+  `status` tinyint(1) DEFAULT '0',
+  `site_id` varchar(50) COLLATE utf8mb4_general_ci DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `ws_site` (
+  `site_id` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`site_id`,`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
